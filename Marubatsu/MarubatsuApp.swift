04b20bdbcdbc9cce7a5edb971cae58c0bb658360
natsumaruta <nabeta0915@gmail.com>
2025-02-15 08:34:40 +0900

    Initial Commit

---
+++ Marubatsu/MarubatsuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarubatsuApp.swift
+//  Marubatsu
+//
+//  Created by 米澤菜摘子 on 2025/02/15.
+//
+
+import SwiftUI
+
+@main
+struct MarubatsuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
